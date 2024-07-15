095ce2ceb129306177a632d179f5836c0cdab32b
RisMouZ <palcailler@gmail.com>
2024-07-15 12:42:48 +0200

    add front

---
+++ app/vite.config.ts
@@ -0,0 +1,23 @@
+// UNCOMMENT FOR MAC
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// WSL USERS ON WINDOWS ONLY (NOT NECESSARY FOR LINUX/MACOS)
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
