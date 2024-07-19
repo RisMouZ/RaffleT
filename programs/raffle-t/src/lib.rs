640f153992a9b91e270b11a150e59ce93c9d3228
RisMouZ <palcailler@gmail.com>
2024-07-19 11:05:52 +0200

    update program

--- programs/raffle-t/src/lib.rs
+++ programs/raffle-t/src/lib.rs
@@ -1,192 +1,185 @@
 use anchor_lang::prelude::*;
-use anchor_lang::system_program;
+// use rand::Rng;
 
+// This is your program's public key and it will update
+// automatically when you build the project.
 declare_id!("");
 
 #[program]
 mod raffle_t {
     use super::*;
 
-    pub fn create_raffle(ctx: Context<CreateRaffle>) -> Result<()> {
-        // require!();
+    pub fn init_user(_ctx: Context<InitUser>) -> Result<()> {
+        _ctx.accounts.user.raffle_count = 0;
+        _ctx.accounts.user.win_count = 0;
 
-        let raffle_account = &mut ctx.accounts.raffle;
-
-        let seller_account = &mut ctx.accounts.seller;
-
-        msg!("Raffle created!",);
-        Ok(())
-    }
-
-    pub fn buy_ticket(ctx: Context<BuyTicket>, amount: u64) -> Result<()> {
-        //require!();
-
-        
-        let raffle_account = &mut ctx.accounts.raffle;
-        let buyer_account = &mut ctx.accounts.buyer;
-        //let raffle_vault = &mut ctx.accounts.raffle_vault;
-        
-
-        let cpi_context = CpiContext::new(
-            ctx.accounts.system_program.to_account_info(),
-            system_program::Transfer {
-                from: ctx.accounts.user.to_account_info(),
-                to: ctx.accounts.raffle_vault.to_account_info(),
-            },
+        msg!(
+            "User account have create {} raffles",
+            _ctx.accounts.user.raffle_count
         );
-        system_program::transfer(cpi_context, amount)?;
-
-        msg!("{} Ticket/s purchased!", amount);
         Ok(())
     }
 
-    pub fn increment_raffle_count(ctx: Context<RaffleCount>) -> Result<()> {
-        let raffle_count = &mut ctx.accounts.raffle_count;
-        raffle_count.count += 1;
+    pub fn create_raffle(
+        _ctx: Context<CreateRaffle>,
+        // nft_id: Pubkey,
+        max_tickets: u32,
+        tickets_price: u32,
+        end_with_deadline: bool,
+        deadline: u32,
+    ) -> Result<()> {
+        let raffle_account = &mut _ctx.accounts.raffle;
+        let user_account = &mut _ctx.accounts.user;
 
-        msg!("Raffle count incremented to: {}!", raffle_count.count);
+        raffle_account.seller = _ctx.accounts.signer.key();
+        // raffle_account.nft_id = nft_id;
+        raffle_account.ticket_price = tickets_price;
+        raffle_account.end_with_deadline = end_with_deadline;
+
+        match end_with_deadline {
+            true => raffle_account.deadline = deadline,
+            false => raffle_account.max_tickets = max_tickets,
+        };
+
+        user_account.raffle_count += 1;
+        msg!(
+            "User account have create {} raffles",
+            user_account.raffle_count
+        );
+
         Ok(())
     }
 
-    pub fn raffle(ctx: Context<RaffleContext>) -> Result<()> {
-        //require!();
+    pub fn buy(_ctx: Context<Buy>) -> Result<()> {
+        require!(
+            _ctx.accounts.signer.key() != _ctx.accounts.raffle.seller,
+            RaffleError::SellerCantBeBuyer
+        );
+        let raffle_account = &mut _ctx.accounts.raffle;
+        let buyer_account = &mut _ctx.accounts.buyer;
 
-        let raffle_account = &mut ctx.accounts.raffle;
-        let seller_account = &mut ctx.accounts.seller;
-        let buyer_account = &mut ctx.accounts.buyer;
-        let ticket_account = &mut ctx.accounts.ticket;
-        //let raffle_vault = &mut ctx.accounts.raffle_vault;
+        buyer_account.buyer_address = _ctx.accounts.signer.key();
 
-        msg!("",);
+        raffle_account.tickets_count += 1;
+
         Ok(())
     }
 
-    pub fn finalize_raffle(ctx: Context<RaffleContext>) -> Result<()> {
-        //require!();
+    pub fn draw(_ctx: Context<Draw>, max_tickets: u32) -> Result<()> {
+        // require!(
+        //     _ctx.accounts.raffle.tickets_count >= _ctx.accounts.raffle.max_tickets,
+        //     RaffleError::RaffleNotEnded
+        // );
 
-        let raffle_account = &mut ctx.accounts.raffle;
-        let seller_account = &mut ctx.accounts.seller;
-        let winner_account = &mut ctx.accounts.buyer;
-        let ticket_winner_account = &mut ctx.accounts.ticket;
+        // let slot = Clock::get()?.slot;
 
-        msg!("Raffle finalized!");
+        // let xorshift_output = xorshift64(slot);
+
+        // let winner = xorshift_output % max_tickets;
+
+        // let winner = rand::thread_rng().gen_range(1..=max_tickets);
+
+        // // msg!("Slot : {}", slot);
+        // // msg!("Xorshift output : {}", xorshift_output);
+        // msg!("Winner : {}", winner);
+
         Ok(())
     }
 }
 
-// PDA Seller + Déclaration du PDA NfT en vente
+//Initialisation du compte obligatoire avant de créer une raffle ou acheter un ticket
+#[derive(Accounts)]
+pub struct InitUser<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 4 + 2 + 1,
+        seeds = [signer.key().as_ref()],
+        bump
+        )]
+    pub user: Account<'info, User>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+//PDA raffle, créer avec le compteur de raffle venant du PDA User et la PubKey du créateur
 #[derive(Accounts)]
 pub struct CreateRaffle<'info> {
-    #[account(init, payer = signer, space = 8 + 1)]
-    pub seller: Account<'info, Seller>,
-
-    #[account(init, payer = signer, space = 8 + 1)] // seeds
+    #[account(mut)]
+    pub user: Account<'info, User>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 32 + 32 + 4 + 4 + 1 + 4 + 4 + 1 + 1,
+        seeds = [&user.raffle_count.to_le_bytes(), signer.key().as_ref()],
+        bump)]
     pub raffle: Account<'info, Raffle>,
-
-    /*#[account(init, payer = signer, space = 8 + 1)] // seeds
-    pub raffle_vault: Account<'info, Raffle>,*/
-
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
-// PDA Buyer
+//PDA de l'acheteur, créer avec le compteur de tickets venant du PDA Raffle et la PubKey de la Raffle
 #[derive(Accounts)]
-pub struct BuyTicket<'info> {
-    #[account(init, payer = user, space = 8 + 1)] // seeds
-    pub buyer: Account<'info, Buyer>,
-
-    #[account(init, payer = user, space = 8 + 1)] // seeds
-    pub ticket: Account<'info, Ticket>,
-
-    #[account(init, payer = signer, space = 8 + 1)] // seeds
-    pub raffle: Account<'info, Raffle>,
-
+pub struct Buy<'info> {
     #[account(mut)]
-    pub user: Signer<'info>,
-
-    /* #[account(mut)]
-    pub raffle_vault: AccountInfo<'info>,*/ 
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct RaffleCount<'info> {
-    #[account(init, payer = signer, space = 8 + 1)] // seeds
-    pub raffle_count: Account<'info, Count>,
-
+    pub raffle: Account<'info, Raffle>,
+    #[account(init,
+    payer = signer,
+    space = 8 + 32 + 1,
+    seeds = [&raffle.tickets_count.to_le_bytes(), raffle.key().as_ref()],
+    bump,)]
+    pub buyer: Account<'info, Buyer>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct RaffleContext<'info> {
-    #[account(mut)] // seeds
-    pub seller: Account<'info, Seller>,
-
-    #[account(mut)] // seeds
-    pub raffle: Account<'info, Raffle>,
-
-    #[account(mut)] // seeds
-    pub buyer: Account<'info, Buyer>,
-
-    #[account(mut)] // seeds
-    pub ticket: Account<'info, Ticket>,
-
-    /*#[account(mut)] // seeds
-    pub raffle_vault: Account<'info, Raffle>,*/
-
+pub struct Draw<'info> {
+    // #[account(mut)]
+    // pub raffle: Account<'info, Raffle>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct Seller {
-    pub seller_name: String,
-    pub seller_id: Pubkey,
-    pub sold_asset: Vec<Raffle>,
-    pub sell_price: u16,
-    pub raffle_deadline: u16,
-    pub ticket_number: u16,
-}
-
-#[account]
-pub struct Buyer {
-    pub buyer_name: String,
-    pub buyer_id: Pubkey,
-    pub purchased_asset: Vec<Raffle>,
-    pub tickets: Vec<Ticket>,
+pub struct User {
+    raffle_count: u32,
+    win_count: u16,
 }
 
 #[account]
 pub struct Raffle {
-    pub raffle_id: Pubkey,
-    pub raffle_name: String,
-    pub raffle_status_on: bool,
-    pub raffle_transfert_to_seller: bool,
-    pub raffle_transfert_to_winner: bool,
-    pub balance: u16,
+    seller: Pubkey,
+    nft_id: Pubkey, //ou u32 ? Il faut regarder la struct d'un nft voir les infos necaissaires
+    max_tickets: u32,
+    ticket_price: u32,
+    end_with_deadline: bool,
+    tickets_count: u32,
+    deadline: u32,
+    raffle_in_progress: bool,
 }
 
 #[account]
-pub struct Ticket {
-    pub ticket_price: u16,
-    pub ticket_id: Pubkey,
-    pub ticket_status: bool,
-    pub ticket_count: u16,
-}
-
-#[account]
-pub struct Count {
-    pub count: u16,
+pub struct Buyer {
+    buyer_address: Pubkey,
 }
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("All tickets are sold")]
-    AllTicketsSold,
-    
+pub enum RaffleError {
+    #[msg("You cannot participate in raffles of which you are the creator.")]
+    SellerCantBeBuyer,
+    #[msg("draw conditions not met")]
+    RaffleNotEnded,
+}
+
+pub fn xorshift64(seed: u64) -> u64 {
+    let mut x = seed;
+    x ^= x << 13;
+    x ^= x >> 7;
+    x ^= x << 17;
+    x
 }
